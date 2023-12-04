4875f8d0e183a269cc9609e760b311d2b6b3e4e5
222mhasan <222mhasan@gmail.com>
2023-12-05 00:56:34 +0600

    upadate 2

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,26 +4,39 @@ const Contact = () => {
         <div className="bg-slate-400 border-2 mt-2">
             <div className="hero min-h-screen bg-base-200">
                 <div className="hero-content flex-col lg:flex-row-reverse">
-                   
-                    <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                        <form className="card-body">
+
+                    <div className="my-10 max-w-3xl shrink-0 w-full rounded-lg shadow-2xl bg-base-100">
+                        <div className="mt-8">
+                            <h1 className="text-5xl font-bold text-center">Contact Me</h1>
+                        </div>
+                        <form className="w-[500px] p-5 ">
                             <div className="form-control">
                                 <label className="label">
-                                    <span className="label-text">Email</span>
+                                    <span className="label-text text-lg font-bold">Name</span>
+                                </label>
+                                <input type="text" placeholder="name" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text text-lg font-bold">Email</span>
                                 </label>
                                 <input type="email" placeholder="email" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
-                                    <span className="label-text">Password</span>
+                                    <span className="label-text text-lg font-bold">Subject</span>
                                 </label>
-                                <input type="password" placeholder="password" className="input input-bordered" required />
-                                <label className="label">
-                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                                </label>
+                                <input type="text" placeholder="subject" className="input input-bordered" required />
                             </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text text-lg font-bold">Message</span>
+                                </label>
+                                <textarea className="textarea textarea-bordered text-lg h-40" placeholder="type your message here"></textarea>
+                            </div>
+
                             <div className="form-control mt-6">
-                                <button className="btn btn-primary">Login</button>
+                                <button className="btn btn-primary">Send</button>
                             </div>
                         </form>
                     </div>
